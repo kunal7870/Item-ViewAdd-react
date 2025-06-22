bf8375c64ffb4bcd09fbd221b7f56fc4890c0596
Kunal verma <kunal33grd@gmail.com>
2025-06-22 06:40:47 +0530

    few changes in link

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function Home() {
     <div className="min-h-[calc(100vh-64px)] flex flex-col justify-center items-center gap-6 bg-gradient-to-b from-gray-900 to-gray-500 text-center px-4">
       <h1 className="text-3xl text-white font-bold">Item Manager</h1>
       <div className="flex gap-4">
-        <a href="/view" className="px-6 py-3 bg-gradient-to-r from-gray-800 to-gray-950 text-white font-semibold rounded shadow hover:brightness-125 transition">View Items</a>
-        <a href="/add" className="px-6 py-3 bg-gradient-to-r from-gray-950 to-gray-800 text-white font-semibold rounded shadow hover:brightness-125 transition">Add Item</a>
+        <Link to="/view" className="px-6 py-3 bg-gradient-to-r from-gray-800 to-gray-950 text-white font-semibold rounded shadow hover:brightness-125 transition">View Items</Link>
+        <Link to="/add" className="px-6 py-3 bg-gradient-to-r from-gray-950 to-gray-800 text-white font-semibold rounded shadow hover:brightness-125 transition">Add Item</Link>
       </div>
     </div>
   );
